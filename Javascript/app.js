3c9921875a8b044a426dbe806aee06c8d48c3bb7
Steptoesrees <Leonhsouthall@gmail.com>
2025-12-02 00:21:56 +0000

    move

--- Javascript/app.js
+++ Javascript/app.js
@@ -5,3 +5,32 @@ function quitPage() {
     
     window.location.href = 'about:blank';}
   }
+
+(function () {
+  const win = document.getElementById('Main');
+  const handle = win.querySelector('.windowtopbar');
+  let offsetX = 0, offsetY = 0, dragging = false;
+
+  handle.addEventListener('mousedown', (e) => {
+    dragging = true;
+    handle.style.cursor = 'grabbing';
+    const rect = win.getBoundingClientRect();
+    offsetX = e.clientX - rect.left;
+    offsetY = e.clientY - rect.top;
+    document.addEventListener('mousemove', onMove);
+    document.addEventListener('mouseup', onUp);
+  });
+
+  function onMove(e) {
+    if (!dragging) return;
+    win.style.left = `${e.clientX - offsetX}px`;
+    win.style.top = `${e.clientY - offsetY}px`;
+  }
+
+  function onUp() {
+    dragging = false;
+    handle.style.cursor = 'grab';
+    document.removeEventListener('mousemove', onMove);
+    document.removeEventListener('mouseup', onUp);
+  }
+})();
